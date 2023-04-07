60d533a742d2c38fe2fc7e7ad383c4bf5ca8e80a
polinaVorob <121984574+polinaVorob@users.noreply.github.com>
2023-04-07 22:49:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,4 +5,4 @@ import numpy as np
 chat_id = 243149489 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    return np.log((x).mean()) - np.log(415)
+    return (4 * np.log((x - 415).mean()) - np.log(((x - 415)**2).mean())) / 2
